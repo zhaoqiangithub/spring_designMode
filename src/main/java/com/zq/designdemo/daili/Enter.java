f9319719c7423f88810fb8732eacba9b9dd34e45
zhaoqianng <17691274108@163.com>
2019-07-17 21:10:17 +0800

    利用jdk 实现简单代理模式，熟悉代理模式流程

--- src/main/java/com/zq/designdemo/daili/Enter.java
+++ src/main/java/com/zq/designdemo/daili/Enter.java
@@ -7,12 +7,26 @@ public class Enter {
 
         //2、自从有了媒婆代理
         try {
+            //获取代理对象
             Person person = (Person)new MeiPo().getInstance(new Zhangsan());
+            System.out.println("代理对象"+person.getClass());
+            //这里这个person是新的代理对象了不是原来的zhangsan 。代理对象调用findTrueLove 是在调用invoke方法
             person.findTrueLove();
 
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
+        /**
+         * 原理
+         * 1、拿到被代理对象的引用，然后获取他的接口
+         * 2、jdk代理重新生成一个类，同时实现代理对象所实现的接口
+         * 3、把被代理对象的引用也拿到
+         * 4、重新动态生成一个class字节码
+         * 5、然后编译
+         */
     }
 }
+
