1b16982a69c88b242ddf62e4feb1e69d823b8564
zhaoqianng <17691274108@163.com>
2019-07-17 21:12:22 +0800

    利用jdk实现简单代理模式 漏提交的proxy

--- src/main/java/com/zq/designdemo/daili/MeiPo.java
+++ src/main/java/com/zq/designdemo/daili/MeiPo.java
@@ -12,6 +12,7 @@ public class MeiPo implements InvocationHandler {
     //获取被代理人的个人资料
     public Object getInstance(Person object) throws Exception{
         this.person = object;
+        System.out.println("原来的代理对象："+object);
         Class<? extends Person> aClass = object.getClass();
         //三个参数  要去代理：第一个参数：classLoader   第二个就是获取他的接口 第三个代理人
         return Proxy.newProxyInstance(aClass.getClassLoader(),aClass.getInterfaces(),this);
@@ -19,9 +20,11 @@ public class MeiPo implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        //此处是代理人做筛选
+
         System.out.println("我是媒婆："+this.person.getName());
         System.out.println("开始海选");
-        System.out.println("如果合适就结婚");
+        System.out.println("也就是代理前的预处理");
         System.out.println("--------");
         this.person.findTrueLove();
         System.out.println("--------");
